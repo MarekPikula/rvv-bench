cbeedc9b55caef9c3df998ad3c0e45e78c9a5171
Marek Pikuła <m.pikula@partner.samsung.com>
2024-06-26 11:27:17 +0000

    Adjust for PULP Ara
    It temporarily removes some instruction tests as they result in illegal
    instruction exceptions or outright hang the processor.

--- config.mk
+++ config.mk
@@ -2,13 +2,13 @@
 WARN=-Wall -Wextra -Wno-unused-function -Wno-unused-parameter
 
 # freestanding using any recent clang build
-CC=clang
-CFLAGS=--target=riscv64 -march=rv64gcv_zba_zbb_zbs -O3 ${WARN} -nostdlib -fno-builtin -ffreestanding
+# CC=clang
+# CFLAGS=--target=riscv64 -march=rv64gcv_zba_zbb_zbs -O3 ${WARN} -nostdlib -fno-builtin -ffreestanding
 #CFLAGS=--target=riscv32 -march=rv32gc_zve32f_zba_zbb_zbs -O3 ${WARN} -nostdlib -fno-builtin -ffreestanding
 
 # full cross compilation toolchain
-#CC=riscv64-linux-gnu-gcc
-#CFLAGS=-march=rv64gcv -O3 ${WARN}
+CC=/opt/riscv/bin/riscv64-unknown-linux-gnu-gcc
+CFLAGS=-march=rv64gcv -O3 -static ${WARN}
 
 # native build
 #CC=cc
